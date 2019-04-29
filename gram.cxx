803e3ddb6dfc8042f15e1a92329c03f0d0012cab
Astarott <47717844+Astarott@users.noreply.github.com>
2019-04-29 23:30:59 +0700

    Imputs

---
+++ gram.cxx
@@ -0,0 +1,20 @@
+
+#encoding "utf-8"
+#GRAMMAR_ROOT All
+
+Ent -> 'свод' 'череп' ;
+
+Ent -> Adj<gnc-agr[1]>+ Noun<gnc-agr[1], rt> ;
+Ent -> Noun<gnc-agr[1]> Adj<gnc-agr[1]>+ ;
+Ent -> Noun ;
+
+Ent -> Noun Ent+ ;
+
+Rel -> Ent interp (Equality.Name1) "являться" Ent interp (Equality.Name2) ;
+Rel -> Ent interp (Equality.Name1) "образовывать" Ent interp (Equality.Name2) ;
+Rel -> Ent interp (Including.Name1) "содержать" Ent interp (Including.Name2) ;
+Rel -> Ent interp (Including.Name1) "вдаваться" "между" Ent interp (Including.Name2) ;
+
+
+All -> Rel; 
+
