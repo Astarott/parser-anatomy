0d8d920439608ac13fc2ef006670a277046e6ff2
Astarott <47717844+Astarott@users.noreply.github.com>
2019-05-02 23:27:40 +0700

    Второй день работы

--- entities.cxx
+++ entities.cxx
@@ -1,24 +1,37 @@
 #encoding "utf-8"
 #GRAMMAR_ROOT All
 
-Entity -> Adj<gnc-agr[1]>* Noun<gnc-agr[1], gram='S', rt> ;
+//Entity -> Adj<gnc-agr[1]>* Noun<gnc-agr[1], gram='S', rt> ;
+//SuperEntity -> Entity Word<kwtype="включение"> Entity;
+//Fraza ->  Word<kwtype="прил",gnc-agr[1]>* Word<kwtype="сущ", rt, gnc-agr[1]>
 
-SuperEntity -> Entity Word<kwtype="включение"> Entity;
+//Объявление сущностей
+Ent -> Word<kwtype="прил",gnc-agr[1]>* Word<kwtype="сущ", rt, gnc-agr[1]>;
+Ent -> Word<kwtype="сущ", gn-agr[1], rt> Word<kwtype="сущ",gn-agr[1],gram="род">+;
 
 
+//Слова включения (Работает, добавить слова в статью и немного дораборать)
+mus -> (Ent<gram="вин">) (',') ('и') (Ent<gram="вин">) (',') ('и') (Ent<gram="вин">) (',') ('и') (Ent<gram="вин">) (',') ('и') Ent<gram="вин">;
+Including ->  Ent interp(Including.Name1) Word<kwtype="включение"> mus interp(Including.Name2);
+//Including ->  Ent interp(Including.Name1) Word<kwtype="включение"> mus interp(Including.Name2);
 
-Including -> Entity interp(Including.Name1) Word<kwtype="включение"> Entity interp(Including.Name2);
-//Entity -> Noun<gnc-agr[1]> Adj<gnc-agr[1]>+ ;
+//Работа с тире (доделать)
+Including -> Ent interp(Including.Name1) '-' Ent interp(Including.Name2);
 
-//Entity -> Noun<rt> Noun<gram='gen'> 'и' Noun<gram='gen'>;
-//Entity -> Noun<rt> Noun<gram='gen'>;
+//Перевод с латинского  (Работает)
+lat -> LBracket AnyWord* Word<lat,wfm=~"[I|V|L|X]{1,10}"> interp(Translate.grec);
+Grec -> lat AnyWord* interp(+Translate.grec) RBracket;
+//Translate -> Ent interp(Translate.ru) Grec;
+Translate -> Word interp(Translate.ru) Grec;
 
-//Entity -> Noun;
+//Уточнения, синонимы и все остальное, что есть в скобках. (Доделать)
+info -> LBracket AnyWord<wfm=~"[)]{1}">+ interp(Sin.ent2) RBracket;
+Sinonim -> Ent interp(Sin.ent1) info;
 
-Final -> Entity interp(Entity.Name);
-
-Final -> Entity interp(Entity.Name);
-
+//Вывод
+//Final -> Ent interp(Entity.Name);
+Final -> mus;
+Final -> Translate;
 Final -> Including;
-
+//Final -> Sinonim;
 All -> Final;
