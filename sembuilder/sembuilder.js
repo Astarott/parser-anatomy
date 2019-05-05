9ec1afa2d2a5e7e4e60cc2f166c159a816127664
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-05-05 12:11:29 +0700

    sembuilder: Reading includings

--- sembuilder/sembuilder.js
+++ sembuilder/sembuilder.js
@@ -35,8 +35,95 @@ function readEquality(nodes)
     return res;
 }
 
+function readFacts(nodes, objFunc)
+{
+    var res = [];
+    for(var i = 0;i < nodes.length;i++)
+    {
+	var obj = objFunc(nodes[i].childNodes());
+	if (obj != null)
+	    res.push(obj);
+    }
+    return res;
+}
+
+
+function readConnection(nodes)
+{
+    var res = [];
+    for(var i = 0;i < nodes.length;i++)
+    {
+	var children = nodes[i].childNodes();
+	var first = "";
+	var second = "";
+	var wth = "";
+//	var opt = "";
+	for(var j = 0;j < children.size();j++)
+	{
+	    var ch = children.get(j);
+	    	    var name = ch.tagName();
+	    var text = ch.attr("val");
+	    switch(name.toLowerCase())
+	    {
+		case "first":
+		first = text;
+		continue;
+		case "second":
+		second = text;
+		continue;
+		case "with":
+		wth = text;
+		continue;
+		default:
+		print("WARNING: Unknown tag in a connection fact: " + name);
+	    }
+	}
+	res.push({first: first, second: second, wth: wth});
+    }
+    return res;
+}
+
 var is = new java.io.FileInputStream("input.xml");
 var doc = org.jsoup.Jsoup.parse(is, "UTF-8", "");
-
 var equality = readEquality(doc.getElementsByTag("equality"));
-//print("Equality facts: " + equality.length);
+var connection = readConnection(doc.getElementsByTag("connection"));
+
+var including = readFacts(doc.getElementsByTag("including"), function(tags)
+{
+	var list = "";
+	var value = "";
+//	var opt = "";
+	for(var j = 0;j < tags.size();j++)
+	{
+	    var tag = tags.get(j);
+	    	    var name = tag.tagName();
+	    var text = tag.attr("val");
+	    switch(name.toLowerCase())
+	    {
+		case "list":
+		list = text;
+		continue;
+		case "value":
+		value = text;
+		continue;
+		default:
+		print("WARNING: Unknown tag in a including fact: " + name);
+	    }
+	}
+    return {in: list, value: value};
+});
+
+/*
+for(var i = 0;i < equality.length;i++)
+    print(equality[i].name1 + "=" + equality[i].name2 + " (" + equality[i].opt + ")");
+*/
+/*
+for(var i = 0;i < connection.length;i++)
+    print(connection[i].first + "=" + connection[i].second + " (" + connection[i].wth + ")");
+*/
+
+for(var i = 0;i < including.length;i++)
+    print(including[i].value + " (в) " + including[i].in );
+
+
+
