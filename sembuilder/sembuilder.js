0ab7a660d3cf2fcd63184b005d9acd33b49b96ad
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-05-05 13:33:10 +0700

    sembuilder: Adding mystem running

--- sembuilder/sembuilder.js
+++ sembuilder/sembuilder.js
@@ -1,4 +1,36 @@
 
+function runMystem(lines)
+{
+    var tmpFile = java.io.File.createTempFile("mystem-", ".tmp");
+    var w = new java.io.BufferedWriter(new java.io.OutputStreamWriter(new java.io.FileOutputStream(tmpFile)));
+    for(var i = 0;i < lines.length;i++)
+    {
+	w.write(lines[i]);
+	w.newLine()
+    }
+    w.flush();
+    w.close();
+    var b = new java.lang.ProcessBuilder("mystem", "-c", "--format=json", tmpFile.getAbsolutePath());
+    var p = b.start();
+    p.getOutputStream().close();
+    var r = new java.io.BufferedReader(new java.io.InputStreamReader(p.getInputStream()));
+    var b = new java.lang.StringBuilder();
+    var line = r.readLine();
+    while(line != null)
+    {
+	b.append(line).append("\n");
+	line = r.readLine();
+    }
+    p.getInputStream().close();
+    tmpFile.delete();
+    return eval(b.toString());
+}
+
+function entity(str)
+{
+    //print(str);
+}
+
 function readFacts(nodes, objFunc)
 {
     var res = [];
@@ -68,45 +100,48 @@ var connection = readFacts(doc.getElementsByTag("connection"), function(tags){
     return {first: first, second: second, wth: wth};
 });
 
-var including = readFacts(doc.getElementsByTag("including"), function(tags)
-			  {
-			      var list = "";
-			      var value = "";
-			      for(var j = 0;j < tags.size();j++)
-			      {
-				  var tag = tags.get(j);
-	    			  var name = tag.tagName();
-				  var text = tag.attr("val");
-				  switch(name.toLowerCase())
-				  {
-				      case "list":
-				      list = text;
-				      continue;
-				      case "value":
-				      value = text;
-				      continue;
-				      default:
-				      print("WARNING: Unknown tag in a including fact: " + name);
-				  }
-			      }
-			      return {in: list, value: value};
-			  });
-
+var including = readFacts(doc.getElementsByTag("including"), function(tags) {
+    var list = "";
+    var value = "";
+    for(var j = 0;j < tags.size();j++)
+    {
+	var tag = tags.get(j);
+	var name = tag.tagName();
+	var text = tag.attr("val");
+	switch(name.toLowerCase())
+	{
+	    case "list":
+	    list = text;
+	    continue;
+	    case "value":
+	    value = text;
+	    continue;
+	    default:
+	    print("WARNING: Unknown tag in a including fact: " + name);
+	}
+    }
+    return {nameIn: list, name: value};
+});
 
 /*
 for(var i = 0;i < equality.length;i++)
-    print(equality[i].name1 + "=" + equality[i].name2 + " (" + equality[i].opt + ")");
-*/
-
-
+{
+    entity(equality[i].name1);
+    entity(equality[i].name2);
+}
 for(var i = 0;i < connection.length;i++)
-    print(connection[i].first + "=" + connection[i].second + " (" + connection[i].wth + ")");
-
-
-/*
+{
+    entity(connection[i].first);
+        entity(connection[i].second);
+}
 for(var i = 0;i < including.length;i++)
-    print(including[i].value + " (в) " + including[i].in );
+{
+    entity(including[i].value);
+    entity(including[i].nameIn);
+}
 */
 
 
 
+var res = runMystem(["Это пробный текст"]);
+print(res.length);
